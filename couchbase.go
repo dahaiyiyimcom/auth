fd098b60027bfc3a19cad4ddd8b53a35162099c9
SevgiF <svg.fdn00@gmail.com>
2025-08-13 14:15:40 +0300

    refactor: allow CouchbaseSession to be provided externally

--- couchbase.go
+++ couchbase.go
@@ -1,9 +1,7 @@
 package auth
 
 import (
-	"fmt"
 	"github.com/couchbase/gocb/v2"
-	"sync"
 	"time"
 )
 
@@ -21,44 +19,4 @@ type CouchbaseStore struct {
 	Cluster    *gocb.Cluster
 	Bucket     *gocb.Bucket
 	Collection *gocb.Collection
-}
-
-var (
-	storeInstance *CouchbaseStore
-	once          sync.Once
-)
-
-func GetCouchbaseStore(config CouchbaseConfig) (*CouchbaseStore, error) {
-	var err error
-	once.Do(func() {
-		cluster, e := gocb.Connect(config.ConnStr, gocb.ClusterOptions{
-			Username: config.Username,
-			Password: config.Password,
-		})
-		if e != nil {
-			err = e
-			return
-		}
-		err = cluster.WaitUntilReady(config.Timeout, nil)
-		if err != nil {
-			panic(fmt.Sprintf("Cluster is not ready or credentials invalid: %v", err))
-		}
-
-		bucket := cluster.Bucket(config.BucketName)
-		if e = bucket.WaitUntilReady(config.Timeout, nil); e != nil {
-			err = e
-			return
-		}
-
-		scope := bucket.Scope(config.Scope)
-		collection := scope.Collection(config.Collection)
-
-		storeInstance = &CouchbaseStore{
-			Cluster:    cluster,
-			Bucket:     bucket,
-			Collection: collection,
-		}
-	})
-
-	return storeInstance, err
 }
